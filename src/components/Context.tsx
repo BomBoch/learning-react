d67c57266ce17df63cf0e1ed5ab60e2c661e1d23
cihat <cihat02020202@gmail.com>
2021-08-26 21:33:32 +0300

    Add context

---
+++ src/components/Context.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useState } from 'react'
+import defaultClasses from './button.default.module.css'
+import greenClasses from './button.green.module.css'
+import redClasses from './button.red.module.css'
+export type ThemeContextType = 'default' | 'red' | 'green'
+export const ThemeContext = createContext('default')
+export const Button = () => {
+  const theme = useContext(ThemeContext)
+  const classes = {
+    default: defaultClasses,
+    red: redClasses,
+    green: greenClasses,
+  }[theme]
+  return (
+    <button type="button" className={classes.button}>
+      themed button
+    </button>
+  )
+}
+export const ThemeSwitcher = ({ children }) => {
+  const [theme, setTheme] = useState('default')
+  return (
+    <>
+      <label>
+        theme
+        <select value={theme} onChange={(e) => setTheme(e.target.value)}>
+          <option>default</option>
+          <option>red</option>
+          <option>green</option>
+        </select>
+      </label>
+      <br />
+      <ThemeContext.Provider value={theme as ThemeContextType}>
+        {children}
+      </ThemeContext.Provider>
+    </>
+  )
+}
+export default (
+  <ThemeSwitcher>
+    <Button />
+  </ThemeSwitcher>
+)
