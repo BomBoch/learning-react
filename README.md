db4fe76453901fdf28836f41d8c877855f6c20a0
cihat <cihat02020202@gmail.com>
2021-08-26 21:34:45 +0300

    Add project

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+# Custom Hooks => ?
+# Advanced Hooks => ?
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
+import State from "./components/State"
+import UseRef from "./components/UseRef"
+import UseEffect from "./components/UseEffect"
+import EventsForms from "./components/EventsForms"
+import FormikComponent from "./components/Formik"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,9 +22,33 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h4>useState</h4>
+      <State />
+      <hr />
+      <br />
+      <h4>useRef</h4>
+      <UseRef />
+      <hr />
+      <br />
+      <h4>useEffect</h4>
+      <UseEffect />
+      <hr />
+      <br />
+      <h4>Evets & Forms</h4>
+      <EventsForms />
+      <hr />
+      <br />
+      <h4>Formik</h4>
+      <FormikComponent />
+      <hr />
+      <br />
+      {/* <h4>UseMemo</h4>
+      <UseMemo />
+      <hr />
+      <br /> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
